e19d8257031deb95c3c89d26588b9c9dbbcc8f27
michael.ivanenko@googlemail.com <hpCYJP2KH36QPdKx>
2020-09-01 13:04:52 +0200

    textinput works

--- app/src/main/java/com/example/pin_compliment/MainActivity.java
+++ app/src/main/java/com/example/pin_compliment/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.pin_compliment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button btn_pin;
+    EditText inputPin;
+    EditText inputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,38 +23,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         btn_pin = findViewById(R.id.btn_confirm);
+        inputText = findViewById(R.id.inputNumber);
+        inputPin = findViewById(R.id.inputText);
 
 
-        btn_pin.setOnClickListener(getPIN);
+        btn_pin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getPIN();
+            }
+        });
     }
 
 
 
-    public void getKompliment(int pin){
-        switch (pin){
-            case 1:
-            default:
-                Context context = getApplicationContext();
-                CharSequence text = "Hello toast!";
-                int duration = Toast.LENGTH_SHORT;
+    public void getKompliment(EditText pin){
 
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
+        String input = pin.getText().toString().trim().toLowerCase();
+        CharSequence text;
+
+        switch (input){
+            case "aaa":
+                text =  "Ich liebe dich!";
                 break;
-
+            case "liebe":
+                text = "я люблю тебя!";
+                break;
+            default:
+                text =  "input prüfen";
         }
 
+
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
     }
 
+    @SuppressLint("WrongViewCast")
     public void getPIN(){
-        int pin = 0;
+        String pwd = "";
 
 
-        getKompliment(pin);
+
+        getKompliment(inputPin);
     }
 
     @Override
     public void onClick(View view) {
-        getPIN();
+
     }
 }
